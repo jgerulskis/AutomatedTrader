714d2e63b2b448f2db52a498ab840a0df66c74a6
Jack Gerulskis <jfgerulskis@wpi.edu>
2020-10-05 12:01:59 -0400

    Initial commit for twitter data collector

---
+++ example_config.py
@@ -0,0 +1,6 @@
+consumer_key=''
+secret_consumer_key=''
+access_key=''
+secret_access_key=''
+
+config.data_path=''
---
+++ main.py
@@ -0,0 +1,49 @@
+import requests
+from requests_oauthlib import OAuth1
+import pandas as pd
+import time
+import config
+
+base_url = 'https://api.twitter.com/'
+auth_url = '{}oauth2/token'.format(base_url)
+auth_resp = OAuth1(config.consumer_key, config.secret_consumer_key, config.access_key, config.secret_access_key)
+
+csv_file = config.data_path
+search_terms = ["$btc", "bitcoin"]
+
+def search(search_terms, MAX_RESULTS=100):
+    url = 'https://api.twitter.com/2/tweets/search/recent?query=bitcoin&tweet.fields=created_at,public_metrics,lang&user.fields=public_metrics&max_results=' + str(MAX_RESULTS)
+    search_results = requests.get(url, auth=auth_resp)
+    print('Server responded with {}'.format(search_results.status_code))
+    if (search_results.status_code == 200):
+        data = search_results.json().get('data')
+        print('Got {} tweets'.format(len(data)))
+        return data
+    else:
+        print('Error on request!')
+        return None
+
+def append_data(json_list):
+    df = pd.DataFrame.from_records(json_list)
+    export_csv = df.to_csv(csv_file, mode='a')
+    print('Exported to ' + csv_file)
+
+"""
+The Twitter API allows 500,000 requests per a month
+
+500,000 tweets a month
+~16,665  tweets a day
+~694     tweets an hour
+~11      tweets a minute
+"""
+total_tweets = 0
+total_time = 0
+while (True):
+    print('\t Total tweets: ' + str(total_tweets))
+    print('\t Total time: ' + str(total_time))
+    data = search(search_terms, MAX_RESULTS=11)
+    if (data != None):
+        total_tweets = len(data)
+        append_data(data)
+    time.sleep(60)
+    total_time += 60
